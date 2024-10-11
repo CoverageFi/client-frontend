63598af94cab9fa80f556bfddf4876dce4729e38
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-10-11 23:28:19 +0530

    added utility functions

---
+++ app/utils/utilityFunctions.ts
@@ -0,0 +1,21 @@
+// // Import and configure the developer-controlled wallet SDK
+// const {
+//   initiateDeveloperControlledWalletsClient,
+// } = require("@circle-fin/developer-controlled-wallets");
+// const circleDeveloperSdk = initiateDeveloperControlledWalletsClient({
+//   apiKey: "<API_KEY>",
+//   entitySecret: "<ENTITY_SECRET>",
+// });
+
+// // Import and configure the smart contract SDK
+// const {
+//   initiateSmartContractPlatformClient,
+// } = require("@circle-fin/smart-contract-platform");
+// const circleContractSdk = initiateSmartContractPlatformClient({
+//   apiKey: "<API_KEY>",
+//   entitySecret: "<ENTITY_SECRET>",
+// });
+
+// export const response = circleContractSdk.getContract({
+//   id: "0189db84-72b7-7fcc-832b-5bf886b9a0ef",
+// });
